35ebd6f1be353604b694341914981e8f177b5a22
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-06-13 17:39:03 -0300

    url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     b64data = base64.b64encode(bytes_data).decode('utf-8')
-    response = requests.post("http://api-service/vec", json={"code": 0, "base64": b64data})
+    response = requests.post("http://35.209.52.21:8082/vec", json={"code": 0, "base64": b64data})
     payload = response.content.decode("utf-8")
     payload = json.loads(payload)
     dataframe = pd.DataFrame(payload["results"])
