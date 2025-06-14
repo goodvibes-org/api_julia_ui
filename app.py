809bc25b6dafb07ce15978d5fa0fb9516626cb8a
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-06-13 21:52:11 -0300

    agrego boton

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ import json
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    b64data = base64.b64encode(bytes_data).decode('utf-8')
-    response = requests.post("http://35.209.52.21:8082/vec", json={"code": 0, "base64": b64data})
-    payload = response.content.decode("utf-8")
-    payload = json.loads(payload)
-    dataframe = pd.DataFrame(payload["results"])
-    st.write(dataframe)
-
+    btn = st.button("Subir archivo")
+    if btn:
+        bytes_data = uploaded_file.getvalue()
+        b64data = base64.b64encode(bytes_data).decode('utf-8')
+        response = requests.post("http://35.209.52.21:8082/vec", json={"code": 0, "base64": b64data})
+        payload = response.content.decode("utf-8")
+        payload = json.loads(payload)
+        dataframe = pd.DataFrame(payload["results"])
+        st.write(dataframe)
